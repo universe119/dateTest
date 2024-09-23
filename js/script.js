59bc947853bb8b871c2d8f91028f46bb25493fb1
universe119 <ruddh119@gmail.com>
2024-09-23 10:07:31 +0900

    시간 실습 완료

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-console.log("test");
+// 미션1 - new Date()를 활용해서 시간,분,초를 각각 span안에 출력
+// 미션2 - 현재 시간이 13시를 넘기면 em안쪽의 'am'을 pm'로 출력
+const title = document.querySelector("h1");
+const [em, spanHr, spanMin, spanSec] = title.children;
 
-/*
+const now = new Date();
+const hr = now.getHours();
+const min = now.getMinutes();
+const sec = now.getSeconds();
+let apm = hr < 13 ? "am" : "pm";
+
+em.innerText = apm;
+spanHr.innerText = hr;
+spanMin.innerText = min;
+spanSec.innerText = sec;
+
+/* 시간 실습 전
+console.log("test");
   new Date() : 시간관련 인스턴스 객체를 생성해주는 생성자
   시간관련 인스턴스 객체 : 다양한 시간관련
 
 */
+/*
 const month = [
 	"January",
 	"February",
@@ -49,3 +65,4 @@ console.log(now.getSeconds());
 // const gmtNow = no.toGMTString();
 const gmtNow = now.toUTCString();
 console.log(gmtNow);
+*/
