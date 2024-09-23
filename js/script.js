17e161777f679598552d3691ffd2a473ed3a84e2
universe119 <ruddh119@gmail.com>
2024-09-23 10:16:51 +0900

    시간 실습 setInterval 추가

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
+// setInterval : 특정 함수를 일정시간마다 반복 호출
+// setInterval(함수, 인터벌시간) : 인터벌간격마다 함수 반복 호출
+
 // 미션1 - new Date()를 활용해서 시간,분,초를 각각 span안에 출력
 // 미션2 - 현재 시간이 13시를 넘기면 em안쪽의 'am'을 pm'로 출력
 const title = document.querySelector("h1");
 const [em, spanHr, spanMin, spanSec] = title.children;
 
-const now = new Date();
-const hr = now.getHours();
-const min = now.getMinutes();
-const sec = now.getSeconds();
-let apm = hr < 13 ? "am" : "pm";
+// 1초마다 시간 출력 반복
+setInterval(() => {
+	const now = new Date();
+	const hr = now.getHours();
+	const min = now.getMinutes();
+	const sec = now.getSeconds();
+	let apm = hr < 13 ? "am" : "pm";
 
-em.innerText = apm;
-spanHr.innerText = hr;
-spanMin.innerText = min;
-spanSec.innerText = sec;
+	em.innerText = apm;
+	spanHr.innerText = hr;
+	spanMin.innerText = min;
+	spanSec.innerText = sec;
+}, 1000);
 
 /* 시간 실습 전
 console.log("test");
