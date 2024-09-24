7a4bf3cea979a945d1646ebc74e10ddd03e004b0
universe119 <ruddh119@gmail.com>
2024-09-24 21:21:10 +0900

    시간구하는 함수, 시간값을 가공해서 세팅해주는 함수로 기능 분리

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-const screen = document.querySelector(".screen");
-const [spanHr, spanMin, spanSec] = screen.children;
+const numbers = document.querySelectorAll(".screen span");
 
-// 1초마다 시간 출력 반복
 setInterval(() => {
-	const now = new Date();
-	const hr = now.getHours();
-	const min = now.getMinutes();
-	const sec = now.getSeconds();
-
-	spanHr.innerText = hr;
-	spanMin.innerText = min;
-	spanSec.innerText = sec;
+	getTime().forEach((num, idx) => setTime(num, idx));
 }, 1000);
+
+// 시간값을 구해서 반환하는 함수
+function getTime() {
+	const now = new Date();
+	let hr = now.getHours();
+	let min = now.getMinutes();
+	let sec = now.getSeconds();
+	return [hr, min, sec];
+}
+
+//반환된 시간값을 DOM에 세팅하는 함수
+function setTime(num, index) {
+	// 전달받은 첫번째 인수값이 숫자면 앞에 "0"붙여 출력 그렇지 않으면 그냥 값 출력
+	num = num < 10 ? "0" + num : num;
+	// 각 span배열에서 두번쨰 인수로 전달받은 index번째 요소에 위에서 가공한 num값을 출력
+	numbers[index].innerText = num;
+}
