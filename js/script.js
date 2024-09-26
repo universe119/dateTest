9f3d13e814a69265ac12c322d2d8e4181533f2f6
universe119 <ruddh119@gmail.com>
2024-09-26 22:17:24 +0900

    type=module방식의 각 파일마다의 변수를 모듈화해서 전역변수 오염 예방 및 설명

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+/*
+  scope : 특정 변수의 정보값에 접근할 수 있는 범위 유효범위 
+
+  전역변수 : 시스템상에서 모두 접근할 수 있는 변수 (window객체에 등록되는 변수)
+
+  지역변수 : 특정 코드 블록안쪽에서 선언된 변수 : 선언된 코드블록 안쪽에서만 접근 가능
+
+  전역변수의 오염 : window객체에 등록된 변수는 다른 스크립트 파일이라고 하더라도 해당 변수값을 공유하므로
+  실수로 A.js에서 B.js에 있는 변수를 변형시킬수 있는 이슈
+
+  script연결시 type='module'이라고 지정하면 해당 파일 안쪽의 선언된 변수는 설사 코드블록 밖에서 선언되었더라도
+  파일자체의 전용 스코프가 설정되어 다른파일에서 해당 파일의 변수 접근이 불가능한 모듈화 상태 유지
+	
+  type='module'지정시 defer와 마찬가지로 스크립트와 body안쪽코드가 비동기적으로(동시에) 해석됨
+*/
+let DB = "엄청중요함정보값"; //전역변수테스트
 const main = document.querySelector("main");
 const numbers = document.querySelectorAll(".screen span");
 const [am, pm] = document.querySelectorAll(".screen em");
